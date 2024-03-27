61b0ef1ca0d80fb7fcf86798ff1223e851e89dd1
danielk872 <juniordanielk872@gmail.com>
2024-03-27 14:09:18 -0400

    list

---
+++ 0x03-python-data_structures/3-print_reversed_list_integer.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+
+def print_reversed_list_integer(my_list=[]):
+    my_list.reverse()
+    for i in my_list:
+        print("{:d}".format(i))
+
+
+if __name__ == "__print_reversed_list_integer__":
+    print_reversed_list_integer()
