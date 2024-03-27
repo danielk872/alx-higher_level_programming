aa98f2d84b15360214f39d16e0bb5a711d28f35b
danielk872 <juniordanielk872@gmail.com>
2024-03-27 14:38:40 -0400

    list

--- 0x03-python-data_structures/3-print_reversed_list_integer.py
+++ 0x03-python-data_structures/3-print_reversed_list_integer.py
@@ -1,8 +1,11 @@
 #!/usr/bin/python3
 def print_reversed_list_integer(my_list=[]):
-    my_list.reverse()
-    for i in my_list:
-        print("{:d}".format(i))
+    if (my_list is None or len(my_list) == 0):
+        return my_list
+    else:
+        my_list.reverse()
+        for i in my_list:
+            print("{:d}".format(i))
 
 
 if __name__ == "__main__":
