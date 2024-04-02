7b0f936d018fef233138296314cc19db3b483050
danielk872 <juniordanielk872@gmail.com>
2024-04-02 15:30:24 -0400

    data_structures

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -3,7 +3,7 @@
 def best_score(a_dictionary):
     if a_dictionary is None:
         return None
-    
+
     max_num = max(a_dictionary.values())
     for key, value in a_dictionary.items():
         if value == max_num:
